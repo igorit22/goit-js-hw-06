ab28ce7278029784969049148fd60c1534089c1c
Igor Yatsenko <igor.yatsenko09@gmail.com>
2023-04-17 22:06:29 +0300

    Add task-01

---
+++ js/task-01.js
@@ -0,0 +1,14 @@
+// Отримуємо список всіх li.item в ul#categories
+const categoriesList = document.querySelectorAll(
+  "#categories > .item"
+);
+
+// Виводимо кількість категорій в консоль
+console.log("Number of categories:", categoriesList.length);
+
+// Перебираємо кожен li.item в списку і виводимо в консоль заголовок та кількість його елементів
+categoriesList.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryElementsCount = category.querySelectorAll("li").length;
+  console.log(`Category: ${categoryName}\nElements: ${categoryElementsCount}`);
+});
