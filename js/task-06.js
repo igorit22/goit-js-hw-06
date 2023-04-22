7bde08db0f1f4918f99660d506f2f1b3f6cb9bf7
Igor Yatsenko <igor.yatsenko09@gmail.com>
2023-04-22 23:00:29 +0300

    Add tasks 05, 06

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const validationInput = document.getElementById('validation-input');
+
+validationInput.addEventListener('blur', function() {
+  const length = validationInput.dataset.length;
+  const value = validationInput.value.trim();
+  const isValid = value.length === Number(length);
+
+  if (isValid) {
+    validationInput.classList.remove('invalid');
+    validationInput.classList.add('valid');
+  } else {
+    validationInput.classList.remove('valid');
+    validationInput.classList.add('invalid');
+  }
+});
