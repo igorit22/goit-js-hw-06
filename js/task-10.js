6bc9e35b7dbc74a83e3badc3f0f9e1abc365e60d
Igor Yatsenko <igor.yatsenko09@gmail.com>
2023-04-24 01:23:23 +0300

    Update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,51 +1,37 @@
-// Отримаємо посилання на елементи із сторінки
-
 const input = document.querySelector("#controls input");
 const createBtn = document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
-const boxesDiv = document.querySelector("#boxes");
+const boxes = document.querySelector("#boxes");
 
-// Функція для створення колекції елементів
+createBtn.addEventListener("click", () => {
+  const amount = input.value;
+  if (
+    amount < parseInt(input.getAttribute("min")) ||
+    amount > parseInt(input.getAttribute("max"))
+  ) {
+    alert(
+      `Будь ласка, введіть значення від ${input.min} до ${input.max}`
+    );
+    return;
+  }
 
-function createBoxes(amount) {
-  const boxes = [];
+  let boxesMarkup = "";
   let size = 30;
 
-  // Створюємо цикл для створення заданої кількості елементів
-
-  for (let i = 0; i < amount; i += 1) {
-    const box = document.createElement("div");
-    box.style.width = `${size}px`;
-    box.style.height = `${size}px`;
-    box.style.backgroundColor = getRandomHexColor();
-    boxes.push(box);
+  for (let i = 0; i < amount; i++) {
+    boxesMarkup += `<div style="background-color: ${getRandomHexColor()}; width: ${size}px; height: ${size}px;"></div>`;
     size += 10;
   }
 
-  // Додаємо фрагмент з елементами до контейнера
+  boxes.insertAdjacentHTML("beforeend", boxesMarkup);
+});
 
-  boxesDiv.append(...boxes);
-}
-
-// Функція для очищення контейнера з елементами (очищуємо HTML-вміст контейнера)
-
-function destroyBoxes() {
-  boxesDiv.innerHTML = "";
-}
-
-// Функція для генерації випадкового кольору у форматі HEX
+destroyBtn.addEventListener("click", () => {
+  boxes.innerHTML = "";
+});
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
-
-// Додаємо обробники подій
-
-createBtn.addEventListener("click", () => {
-  createBoxes(input.value);
-  input.value = "";
-});
-
-destroyBtn.addEventListener("click", destroyBoxes);
