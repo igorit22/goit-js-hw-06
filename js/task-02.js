7db87bd86587ae843375ad47313a296a8bfb95ed
Igor Yatsenko <igor.yatsenko09@gmail.com>
2023-04-24 00:17:57 +0300

    Update task-02

--- js/task-02.js
+++ js/task-02.js
@@ -7,17 +7,13 @@ const ingredients = [
   "Condiments",
 ];
 
-// Отримуємо список ul#ingredients
-const ingredientsList = document.querySelector("#ingredients");
+const ul = document.querySelector("#ingredients");
 
-// Перебираємо масив ingredients і для кожного елемента створюємо новий елемент li та додаємо до списку ul#ingredients
-ingredients.forEach((ingredient) => {
-  // Створюємо новий елемент li
+const liArray = ingredients.map((ingredient) => {
   const li = document.createElement("li");
-  // Встановлюємо текстовий вміст елемента li на основі значення з масиву ingredients
   li.textContent = ingredient;
-  // Додаємо клас "item" до елемента li
   li.classList.add("item");
-  // Додаємо елемент li до списку ul#ingredients
-  ingredientsList.appendChild(li);
+  return li;
 });
+
+ul.append(...liArray);
