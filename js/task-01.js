76fcbf4cdf55daf9879af9fd54a909ee5793051a
Igor Yatsenko <igor.yatsenko09@gmail.com>
2023-04-23 23:58:17 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,34 @@
+// // Отримуємо список всіх li.item в ul#categories
+// const categoriesList = document.querySelectorAll(
+//   "#categories > .item"
+// );
+
+// // Виводимо кількість категорій в консоль
+// console.log("Number of categories:", categoriesList.length);
+
+// // Перебираємо кожен li.item в списку і виводимо в консоль заголовок та кількість його елементів
+// categoriesList.forEach((category) => {
+//   const categoryName = category.querySelector("h2").textContent;
+//   const categoryElementsCount =
+//     category.querySelectorAll("li").length;
+//   console.log(
+//     `Category: ${categoryName}\nElements: ${categoryElementsCount}`
+//   );
+// });
+
+
+
 // Отримуємо список всіх li.item в ul#categories
-const categoriesList = document.querySelectorAll(
-  "#categories > .item"
-);
+const categoriesUl = document.getElementById("categories");
+const categoriesList = categoriesUl.children;
 
 // Виводимо кількість категорій в консоль
-console.log("Number of categories:", categoriesList.length);
+console.log(`Number of categories: ${categoriesList.length}`);
 
 // Перебираємо кожен li.item в списку і виводимо в консоль заголовок та кількість його елементів
-categoriesList.forEach((category) => {
-  const categoryName = category.querySelector("h2").textContent;
-  const categoryElementsCount =
-    category.querySelectorAll("li").length;
-  console.log(
-    `Category: ${categoryName}\nElements: ${categoryElementsCount}`
-  );
-});
+for (let i = 0; i < categoriesList.length; i++) {
+  const category = categoriesList[i];
+  const categoryName = category.firstElementChild.textContent;
+  const categoryElementsCount = category.lastElementChild.children.length;
+  console.log(`Category: ${categoryName}\nElements: ${categoryElementsCount}`);
+}
